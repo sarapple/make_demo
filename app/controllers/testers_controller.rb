73571b33a83b2c99035d62ef93bd82c975c5d4cb
Sara Wong <swong2@wellesley.edu>
2015-02-11 10:03:12 -0800

    added user select option to form for blog

--- app/controllers/testers_controller.rb
+++ app/controllers/testers_controller.rb
@@ -4,7 +4,7 @@ class TestersController < ApplicationController
 		@stateTable = Make.table.model('State').now!
 		@userForm = Make.form.model('User').select('state_id', (1..5).select, assoc=true).now!
 		@userTable = Make.table.model('User').now!
-		@blogForm = Make.form.model('Blog').default('user_id', 1).now!
+		@blogForm = Make.form.model('Blog')..select('user_id', (1..4).select, assoc=true).now!
 		@blogTable = Make.table.model('Blog').now!
 	end
 	def inputted
